74c5a8cf00c70deaddcbcd5401e9d009dad7d652
Rajiv Navada <rajivnavada@gmail.com>
2012-02-14 16:54:44 -0800

    Reverting the files in the js directory to their original state.

--- js/bootstrap-transition.js
+++ js/bootstrap-transition.js
@@ -15,24 +15,37 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * ========================================================== */!function($) {
-    $(function() {
-        "use strict";
-        $.support.transition = function() {
-            var thisBody = document.body || document.documentElement, thisStyle = thisBody.style, support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined;
-            return support && {
-                end: function() {
-                    var transitionEnd = "TransitionEnd";
-                    if ($.browser.webkit) {
-                        transitionEnd = "webkitTransitionEnd";
-                    } else if ($.browser.mozilla) {
-                        transitionEnd = "transitionend";
-                    } else if ($.browser.opera) {
-                        transitionEnd = "oTransitionEnd";
-                    }
-                    return transitionEnd;
-                }()
-            };
-        }();
-    });
-}(window.jQuery);
+ * ========================================================== */
+
+!function( $ ) {
+
+  $(function () {
+
+    "use strict"
+
+    /* CSS TRANSITION SUPPORT (https://gist.github.com/373874)
+     * ======================================================= */
+
+    $.support.transition = (function () {
+      var thisBody = document.body || document.documentElement
+        , thisStyle = thisBody.style
+        , support = thisStyle.transition !== undefined || thisStyle.WebkitTransition !== undefined || thisStyle.MozTransition !== undefined || thisStyle.MsTransition !== undefined || thisStyle.OTransition !== undefined
+
+      return support && {
+        end: (function () {
+          var transitionEnd = "TransitionEnd"
+          if ( $.browser.webkit ) {
+          	transitionEnd = "webkitTransitionEnd"
+          } else if ( $.browser.mozilla ) {
+          	transitionEnd = "transitionend"
+          } else if ( $.browser.opera ) {
+          	transitionEnd = "oTransitionEnd"
+          }
+          return transitionEnd
+        }())
+      }
+    })()
+
+  })
+  
+}( window.jQuery )
